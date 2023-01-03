34a065c6e7461b1000581663ec19f81527be706f
Les Matheson <lmatheson4@bloomberg.net>
2023-01-03 11:49:34 -0500

    repub.test 1

--- bin/pubtest-version.sh
+++ bin/pubtest-version.sh
@@ -2,7 +2,7 @@
 
 # Running pubtest-version.sh is the correct way to
 # get the home install path for the tool
-KitVersion=0.1.7
+KitVersion=0.1.8
 
 canonpath() {
     builtin type -t realpath.sh &>/dev/null && {
